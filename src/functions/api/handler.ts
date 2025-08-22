21d1f37fe43e7a68fb9f502fcdc1617e2bfdc632
Andrew Pettigrew <devdrew10@gmail.com>
2025-08-22 11:08:38 -0500

    feat: add dynamodb table notes

--- src/functions/api/handler.ts
+++ src/functions/api/handler.ts
@@ -24,3 +24,61 @@ export const createNote = async (event: APIGatewayProxyEvent, context: Context, 
 
     callback(null, response);
 };
+
+export const updateNote = async (event: APIGatewayProxyEvent, context: Context, callback: Callback<APIGatewayProxyResult>): Promise<void> => {
+    console.log('Event:', event);
+
+    const response: APIGatewayProxyResult = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+    };
+
+    callback(null, response);
+};
+
+
+export const deleteNote = async (event: APIGatewayProxyEvent, context: Context, callback: Callback<APIGatewayProxyResult>): Promise<void> => {
+    console.log('Event:', event);
+
+    const response: APIGatewayProxyResult = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+    };
+
+    callback(null, response);
+};
+
+export const fetchNoteById = async (event: APIGatewayProxyEvent, context: Context, callback: Callback<APIGatewayProxyResult>): Promise<void> => {
+    console.log('Event:', event);
+
+    const response: APIGatewayProxyResult = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+    };
+
+    callback(null, response);
+};
+
+
+export const fetchNotes = async (event: APIGatewayProxyEvent, context: Context, callback: Callback<APIGatewayProxyResult>): Promise<void> => {
+    console.log('Event:', event);
+
+    const response: APIGatewayProxyResult = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+    };
+
+    callback(null, response);
+};
